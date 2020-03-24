02f4e266a9a2ba38c20bb058d8f5d83ed725ce5d
svafiadis1 <57498741+svafiadis1@users.noreply.github.com>
2020-03-24 14:05:22 -0400

    turn  on live server !!!

---
+++ .vscode/index.js
@@ -0,0 +1,61 @@
+
+/*let string = "strings";
+let sentence = "Hello" + "Junshen";
+let  sentence2 = " and Ema" ;
+*/
+
+//console.log(sentence + sentence2);
+/*
+let student1 = "Sandra" ;
+let student2 = "Kelly" ;
+let courseName = "AP Computer Science" ;
+let teacherName = "Whalen" ;
+
+const longSentence = `${student1} and ${student2} are amazing and will be taking ${courseName} with ${teacherName}` ; 
+//Template Literal or Template String
+*/
+
+/*
+//Numbers or integers
+let year = 2020;
+let nextYear = 2020 + 1;
+let past = 2020 / 2;
+let farFuture = 2020 * 50;
+*/
+
+/*
+
+//Booleans
+let trueStatement = true;
+let falseStatement = false;
+*/
+
+//Null and undefined
+//let undefined;
+//let Ron; //let Ron but not defined Ron
+//console.log(Ron);
+//console.log(Sarah);
+
+/*
+//////Boolean Logic
+let student = "Caroline"
+let age = 15;
+//console.log(age === "15");
+//console.log (age < 17);
+console.log(age <= 15 && age > 19);
+*/
+
+/////If statements
+/*
+let school = "High Sc";
+if (school === "Elementary"){
+    console.log("You are in k-5");
+} else if (school === "Middle") {
+    console.log("You are in 6, 7, or 8th grade");
+} else {
+    console.log("you are in HS");
+}
+*/
+
+let ron = "Ron";
+console.log(ron !== "Ema");
